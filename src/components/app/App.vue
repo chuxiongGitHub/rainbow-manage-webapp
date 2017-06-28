ba9add9ab9110c72a41800d49bd95bfd6a6cf9b3
chencaihong <836908728@qq.com>
2017-06-28 15:57:40 +0800

    完善功能

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -3,7 +3,9 @@
     div
       Menu(mode="horizontal" active-name="1")
         .layout-assistant
-          Menu-item(name="1") 一级导航
+          Menu-item(name="1")
+            Icon(type="ios-paper")
+            | 一级导航
           Menu-item(name="2") 二级导航
           Menu-item(name="3") 三级导航
 </template>

323e736f64669714f944056cba3ad8c3e262c7dc
chencaihong <836908728@qq.com>
2017-06-28 13:07:05 +0800

    加入styl正常

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,4 +1,13 @@
 <template lang="pug">
-  .panel
-    Button(type="primary") 查询
+  .layout
+    div
+      Menu(mode="horizontal" active-name="1")
+        .layout-assistant
+          Menu-item(name="1") 一级导航
+          Menu-item(name="2") 二级导航
+          Menu-item(name="3") 三级导航
 </template>
+<style lang="stylus" scoped>
+@import './style/app'
+</style>
+
